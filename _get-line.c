cedbf8f9f6fe6568d2f9a6cdc4b9f4e3c90cdc16
N <51336368+Nmcleon@users.noreply.github.com>
2023-08-07 18:18:55 +0200

    seg

--- _get-line.c
+++ _get-line.c
@@ -1,9 +1,10 @@
 #include "header.h"
 
-char* _getmyline(){
-	char* buf = NULL;
-	size_t n = 0;
+char* _getmyline(void)
+{
+    char* buf = NULL;
+    size_t n = 0;
 
-	_getmyline(&buf, &n, stdin);
-	return buf;
+    getline(&buf, &n, stdin);
+    return (buf);
 }
