8c5e39e8cede17202f8dfcd78a642b57323b7546
N <51336368+Nmcleon@users.noreply.github.com>
2023-08-07 18:14:10 +0200

    seg failure

--- _fork.c
+++ _fork.c
@@ -1,36 +1,36 @@
 #include "header.h"
 
-void _fork(char* buffer){
-	buffer[strcspn(buffer, "\n")] = '\0';
-	if (strlen(buffer) == 0 || strspn(buffer, " \t\n") == strlen(buffer)){
-		_print("no input. \n");
-		exit(EXIT_SUCCESS);
-	}
-	
-	char setbuffer [BUFFER_SIZE];
-		/*if(fgets(setbuffer, BUFFER_SIZE, stdin) == NULL){
-			printf("NO INPUT\n");
-		}*/
+void _fork(char* buffer)
+{
+    buffer[strcspn(buffer, "\n")] = '\0';
+    if (strlen(buffer) == 0 || strspn(buffer, " \t\n") == strlen(buffer))
+    {
+        _print("no input. \n");
+        exit(EXIT_SUCCESS);
+    }
 
-		setbuffer[strcspn(setbuffer, "\n")] = '\0';
-		if (strcmp(buffer, "exit\n") == 0){
-			printf("exit");
-			exit(EXIT_SUCCESS);
+    buffer[strcspn(buffer, "\n")] = '\0';
+    if (strcmp(buffer, "exit") == 0)
+    {
+        printf("exit\n");
+        exit(EXIT_SUCCESS);
+    }
 
-		}
-		pid_t pid = fork();
+    pid_t pid = fork();
 
-		if (pid < 0){
-			fprintf(stderr, "fork failed");
-		}
-
-		else if(pid == 0){
-			execlp(setbuffer, setbuffer, NULL);
-			fprintf(stderr, "command '%s' not found\n", setbuffer);
-			exit(EXIT_FAILURE);
-		}
-		else{
-			int status;
-			waitpid(pid, &status, 0);
-		}
+    if (pid < 0)
+    {
+        fprintf(stderr, "fork failed");
+    }
+    else if (pid == 0)
+    {
+        execlp(buffer, buffer, NULL);
+        fprintf(stderr, "command '%s' not found\n", buffer);
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        int status;
+        waitpid(pid, &status, 0);
+    }
 }
